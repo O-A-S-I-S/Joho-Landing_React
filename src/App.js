c0a103927fe1eb2a6d9760c015cadcc58f064649
Roberto Basauri <89091501+RobertoCarlosBasauriQuispe@users.noreply.github.com>
2022-11-16 22:37:44 -0500

    Feat View The Team
    View The Team for landing page adapted to React.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 import Header from "./Header.js";
 import Home from "./Home.js";
 import AboutSearch from "./AboutSearch.js";
@@ -6,23 +7,26 @@ import AboutCategory from "./AboutCategory.js";
 import AboutUI from "./AboutUI.js";
 import RightNavBar from "./RightNavBar.js";
 import Footer from "./Footer.js";
+import {Team} from "./Team.js";
 
 function App() {
-  let teamView = false;
-
-  const toggleTeamView = () => (teamView = !teamView);
-
+  const [teamView,setTeamView]=useState(false);
+  function toggleTeamView(){
+    setTeamView(!teamView);
+  }
   return (
     <div className="App">
-      <Header toggleTeamView={toggleTeamView} />
+      <Header toggleTeamView={()=>toggleTeamView()} />
       <main>
+        {!teamView?
         <div class="main">
           <Home />
           <AboutSearch />
           <AboutCategory />
           <AboutUI />
           <RightNavBar />
-        </div>
+        </div>:<Team />
+        }
       </main>
       <Footer />
     </div>
